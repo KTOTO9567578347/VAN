4e8779ea8260426d6d6e1ca0311258000674d3d3
Kazakov Viacheslav <kslava2309@gmail.com>
2025-02-24 10:57:00 +0300

    Remake interface

--- system/interface.py
+++ system/interface.py
@@ -32,37 +32,41 @@ class main_win:
 		self.image_widget = tk.Label(self.app) 
 		self.image_widget.pack() 
 
+		self.MainCanvas = tk.Canvas(master = self.app, height=400, width=2000)
+		self.MainCanvas.pack(anchor=tk.CENTER, expand=True)
+
 		self.table_columns = ('ID', 'face_emotion', 'pose_emotion')
 		self.table = ttk.Treeview(self.app, columns= self.table_columns, show = 'headings')
 		self.table.heading('ID', text = 'ID человека')
 		self.table.heading('face_emotion', text = 'Эмоция лица')
 		self.table.heading('pose_emotion', text = 'Эмоция позы')
 		self.table_data = []
-		self.table.pack()
+		self.MainCanvas.create_window((10, 10), window = self.table, anchor= tk.NW)
 
 		self.selected_camera = tk.StringVar()
 		self.cameras_list_widget = ttk.Combobox(self.app, width = 27, textvariable = self.selected_camera)
-		self.cameras_list_widget.pack()
+		self.MainCanvas.create_window((650, 10), window = self.cameras_list_widget, anchor= tk.NW)
 
 		self.cam_recap_but_widget = ttk.Button(text = 'захват видео выбранной камеры', command= self.cv.recap_camera)
-		self.cam_recap_but_widget.pack()
+		self.MainCanvas.create_window((650, 150), window = self.cam_recap_but_widget, anchor= tk.NW)
 
 		self.cam_list_update_but_widget = ttk.Button(text = 'обновить список камер', command= self.cv.update_camera_list)
-		self.cam_list_update_but_widget.pack()
+		self.MainCanvas.create_window((650, 200), window = self.cam_list_update_but_widget, anchor= tk.NW)
 
 		self.return_but = ttk.Button(text = 'Вернуться к стартовому окну', command=self.go_to_start)
-		self.return_but.pack()
+		self.MainCanvas.create_window((10, 280), window = self.return_but, anchor= tk.NW)
 
 		self.is_on_air = tk.BooleanVar()
-		self.is_on_air_widget_checkbox = tk.Checkbutton(self.app, text = 'записывать?', variable= self.is_on_air, onvalue=1, offvalue=0, command = self.cv.start_record)
-		self.is_on_air_widget_checkbox.pack()
+		self.is_on_air_widget_checkbox = tk.Checkbutton(self.app, text = 'записывать видео?', variable= self.is_on_air, onvalue=1, offvalue=0, command = self.cv.start_record)
+		self.MainCanvas.create_window((650, 250), window = self.is_on_air_widget_checkbox, anchor= tk.NW)
 
 		self.is_audio_on_air = tk.BooleanVar()
 		self.is_audio_on_air_widget_checkbox = tk.Checkbutton(self.app, text = 'использовать микрофон для анализа?', variable= self.is_audio_on_air, onvalue=1, offvalue=0, command = self.audio_processor.audio_relay)
-		self.is_audio_on_air_widget_checkbox.pack()
+		self.MainCanvas.create_window((200, 280), window = self.is_audio_on_air_widget_checkbox, anchor= tk.NW)
 
 		self.voice_class_text = tk.Label(self.app, text='-')
-		self.voice_class_text.pack()
+		self.MainCanvas.create_window((300, 250), window=self.voice_class_text)
+
 		self.refresh_voice_class("-")
 
 		#self.play_sound_button_widget = ttk.Button(text = "Проиграть звук", command= self.audio_processor.play_sound)
@@ -105,7 +109,7 @@ class start_win:
 		self.logo_widget = tk.Label(self.start_window, image=photo_image)
 		self.logo_widget.pack()
 
-		self.authors = tk.Label(self.start_window, text="Система распознавания эмоций VAN \n Разработано Дата Квантумом Кванториума СВДЖД \n Авторы: \n Казаков Вячеслав \n Поморцев Дмитрий \n Иванов Кирилл \n Холодов Иван \n Наставник: Матанцев Анатолий Александрович")
+		self.authors = tk.Label(self.start_window, text="Система распознавания эмоций VAN \n Разработано Дата Квантумом Кванториума СВДЖД \n Инструкции и информация: \n https://github.com/KTOTO9567578347/VAN/tree/master")
 		self.authors.pack()
 
 		start_but = ttk.Button(text = 'ЗАПУСК', master= self.start_window, command= lambda: self.start_main())
